6e3860c80ad61131a51af0abfa2e1c5edb96ebb1
DimitarChristoff <christoff@gmail.com>
2013-03-19 20:18:57 +0000

    micro optimising drawLines due to profiler

--- js/chrt.js
+++ js/chrt.js
@@ -907,13 +907,18 @@
 				animationLoop(config, drawScale, drawLines, ctx);
 
 				function drawLines(animPc){
-					for (var i = 0; i < data.datasets.length; i++){
+					var i = 0,
+						len = data.datasets.length,
+						len2,
+						j;
+
+					for (; i < len; i++){
 						ctx.strokeStyle = data.datasets[i].strokeColor;
 						ctx.lineWidth = config.datasetStrokeWidth;
 						ctx.beginPath();
 						ctx.moveTo(yAxisPosX, xAxisPosY - animPc * (calculateOffset(data.datasets[i].data[0], calculatedScale, scaleHop)))
 
-						for (var j = 1; j < data.datasets[i].data.length; j++){
+						for (j = 1, len2 = data.datasets[i].data.length; j < len2; j++){
 							if (config.bezierCurve){
 								ctx.bezierCurveTo(xPos(j - 0.5), yPos(i, j - 1), xPos(j - 0.5), yPos(i, j), xPos(j), yPos(i, j));
 							}
@@ -921,6 +926,7 @@
 								ctx.lineTo(xPos(j), yPos(i, j));
 							}
 						}
+
 						ctx.stroke();
 						if (config.datasetFill){
 							ctx.lineTo(yAxisPosX + (valueHop * (data.datasets[i].data.length - 1)), xAxisPosY);
@@ -932,13 +938,14 @@
 						else {
 							ctx.closePath();
 						}
+
 						if (config.pointDot){
 							ctx.fillStyle = data.datasets[i].pointColor;
 							ctx.strokeStyle = data.datasets[i].pointStrokeColor;
 							ctx.lineWidth = config.pointDotStrokeWidth;
-							for (var k = 0; k < data.datasets[i].data.length; k++){
+							for (j = 0; j < len2; j++){
 								ctx.beginPath();
-								ctx.arc(yAxisPosX + (valueHop * k), xAxisPosY - animPc * (calculateOffset(data.datasets[i].data[k], calculatedScale, scaleHop)), config.pointDotRadius, 0, Math.PI * 2, true);
+								ctx.arc(yAxisPosX + (valueHop * j), xAxisPosY - animPc * (calculateOffset(data.datasets[i].data[j], calculatedScale, scaleHop)), config.pointDotRadius, 0, Math.PI * 2, true);
 								ctx.fill();
 								ctx.stroke();
 							}
